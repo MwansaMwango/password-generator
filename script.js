b537d32d49b659d58cbcbc6cab06eb6a6d435000
Mwansa <bamboworld@yahoo.co.uk>
2020-02-23 17:08:13 +0800

    Added password criteria form

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // Collect user password criteria details
+  var pwdLength = prompt ("Enter password length: (MUST contain between 8 and 128 charaters)");
+  var pwdChartype = prompt () // use checkbox
+
+  return (num * 10);
+} // returns password
